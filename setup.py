5cb546a8a9f30a042037fb30d32d1b94c74387b4
Tom Taylor <tom@tommyt.co.uk>
2016-08-18 15:03:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='consuler',
+    version='0.0.1',
+    description='A basic client for Consul',
+    author='Tom Taylor',
+    author_email='tom+pypi@tomm.yt',
+    url='https://github.com/t0mmyt/python-consuler',
+    packages=['consuler'],
+)
